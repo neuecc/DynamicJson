7a97986167fb1188cfa6504e5377a14270263308
neuecc <neuecc@localhost>
2010-04-30 20:52:43 +0900

    1st Release

--- DynamicJson.cs
+++ DynamicJson.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿/*--------------------------------------------------------------------------
+* DynamicJson
+* ver 1.0.0.0 (Apr. 30th, 2010)
+*
+* created and maintained by neuecc <ils@neue.cc>
+* licensed under Microsoft Public License(Ms-PL)
+* http://neue.cc/
+* http://dynamicjson.codeplex.com/
+*--------------------------------------------------------------------------*/
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
@@ -19,7 +29,24 @@ namespace Codeplex.Data
             @string, number, boolean, @object, array, @null
         }
 
-        #region Reader
+        // public static methods
+
+        /// <summary>from JsonSring to DynamicJson</summary>
+        public static dynamic Parse(string json)
+        {
+            using (var jsonReader = JsonReaderWriterFactory.CreateJsonReader(Encoding.Unicode.GetBytes(json), XmlDictionaryReaderQuotas.Max))
+            {
+                return ToValue(XElement.Load(jsonReader));
+            }
+        }
+
+        /// <summary>create JsonSring from primitive or IEnumerable or Object({public property name:property value})</summary>
+        public static string Serialize(object obj)
+        {
+            return CreateJsonString(new XStreamingElement("root", CreateTypeAttr(GetJsonType(obj)), CreateJsonNode(obj)));
+        }
+
+        // private static methods
 
         private static dynamic ToValue(XElement element)
         {
@@ -33,79 +60,14 @@ namespace Codeplex.Data
                 case JsonType.@string:
                     return (string)element;
                 case JsonType.@object:
-                    return new DynamicJson(element);
+                    return new DynamicJson(element, type);
                 case JsonType.array:
-                    return element.Elements().Select(x => ToValue(x)).ToArray();
+                    return new DynamicJson(element, type);
                 case JsonType.@null:
                 default:
                     return null;
             }
         }
-
-        /// <summary>from JsonSring to DynamicJson</summary>
-        public static dynamic Parse(string json)
-        {
-            using (var jsonReader = JsonReaderWriterFactory.CreateJsonReader(Encoding.Unicode.GetBytes(json), XmlDictionaryReaderQuotas.Max))
-            {
-                return ToValue(XElement.Load(jsonReader));
-            }
-        }
-
-        // represents JavaScript Object
-
-        readonly XElement xml;
-        string jsonCache;
-
-        public DynamicJson(XElement element)
-        {
-            xml = element;
-        }
-
-        public bool IsDefined(string name)
-        {
-            return (xml.Element(name) != null);
-        }
-
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
-        {
-            var element = xml.Element(binder.Name);
-            if (element == null)
-            {
-                result = null;
-                return false;
-            }
-
-            result = ToValue(element);
-            return true;
-        }
-
-        public override IEnumerable<string> GetDynamicMemberNames()
-        {
-            return xml.Elements().Select(x => x.Name.LocalName);
-        }
-
-        public override string ToString()
-        {
-            if (jsonCache != null) return jsonCache;
-
-            // <foo type="null"></foo> is can't serialize. replace to <foo type="null" />
-            foreach (var elem in xml.Descendants().Where(x => x.Attribute("type").Value == "null"))
-            {
-                elem.RemoveNodes();
-            }
-            using (var ms = new MemoryStream())
-            using (var writer = JsonReaderWriterFactory.CreateJsonWriter(ms, Encoding.Unicode))
-            {
-                new XStreamingElement("root", new XAttribute("type", "object"), xml.Elements()).WriteTo(writer);
-                writer.Flush();
-                jsonCache = Encoding.Unicode.GetString(ms.ToArray());
-                return jsonCache;
-            }
-        }
-
-        #endregion
-
-        #region Writer
 
         private static JsonType GetJsonType(object obj)
         {
@@ -179,10 +141,8 @@ namespace Codeplex.Data
                 .Select(a => new XStreamingElement(a.Name, CreateTypeAttr(GetJsonType(a.Value)), CreateJsonNode(a.Value)));
         }
 
-        /// <summary>return JsonSring from primitive or IEnumerable or Object(Key:public property name, Value:property value)</summary>
-        public static string Save(object obj)
+        private static string CreateJsonString(XStreamingElement element)
         {
-            var element = new XStreamingElement("root", CreateTypeAttr(GetJsonType(obj)), CreateJsonNode(obj));
             using (var ms = new MemoryStream())
             using (var writer = JsonReaderWriterFactory.CreateJsonWriter(ms, Encoding.Unicode))
             {
@@ -192,6 +152,197 @@ namespace Codeplex.Data
             }
         }
 
-        #endregion
+        // dynamic structure represents JavaScript Object/Array
+
+        readonly XElement xml;
+        readonly JsonType jsonType;
+
+        /// <summary>create blank JSObject</summary>
+        public DynamicJson()
+        {
+            xml = new XElement("root", CreateTypeAttr(JsonType.@object));
+            jsonType = JsonType.@object;
+        }
+
+        private DynamicJson(XElement element, JsonType type)
+        {
+            Debug.Assert(type != JsonType.@object || type != JsonType.array);
+
+            xml = element;
+            jsonType = type;
+        }
+
+        public bool IsObject { get { return jsonType == JsonType.@object; } }
+
+        public bool IsArray { get { return jsonType == JsonType.array; } }
+
+        public int Length { get { return GetDynamicMemberNames().Count(); } }
+
+        /// <summary>has property or not</summary>
+        public bool IsDefined(string name)
+        {
+            return IsObject && (xml.Element(name) != null);
+        }
+
+        /// <summary>delete property</summary>
+        public void Delete(string name)
+        {
+            var elem = xml.Element(name);
+            if (elem != null) elem.Remove();
+        }
+
+        private object DeserializeObject(Type targetType)
+        {
+            var result = Activator.CreateInstance(targetType);
+            var dict = targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .ToDictionary(pi => pi.Name, pi => pi);
+            foreach (var item in xml.Elements())
+            {
+                PropertyInfo propertyInfo;
+                if (!dict.TryGetValue(item.Name.LocalName, out propertyInfo)) continue;
+                var value = ToValue(item);
+                if (value is DynamicJson)
+                {
+                    var json = ((DynamicJson)value);
+                    value = (json.IsArray)
+                        ? json.DeserializeArray(propertyInfo.PropertyType)
+                        : json.DeserializeObject(propertyInfo.PropertyType);
+                }
+                propertyInfo.SetValue(result, Convert.ChangeType(value, propertyInfo.PropertyType), null);
+            }
+            return result;
+        }
+
+        private object DeserializeArray(Type targetType)
+        {
+            
+            if (targetType.IsArray)
+            {
+                var elemType = targetType.GetElementType();
+                dynamic array = Array.CreateInstance(elemType, xml.Elements().Count());
+                var index = 0;
+                foreach (var item in xml.Elements()) array[index++] = Convert.ChangeType(ToValue(item),elemType);
+                return array;
+            }
+            else
+            {
+                var elemType = targetType.GetGenericArguments()[0];
+                dynamic list = Activator.CreateInstance(targetType);
+                foreach (var item in xml.Elements()) list.Add(Convert.ChangeType(ToValue(item), elemType));
+                return list;
+            }
+        }
+
+        /// <summary>mapping by Public PropertyName</summary>
+        public T Deserialize<T>() where T : new()
+        {
+            if (IsArray) throw new InvalidOperationException();
+
+            return (T)DeserializeObject(typeof(T));
+        }
+
+        private bool TryGet(XElement element, out object result)
+        {
+            if (element == null)
+            {
+                result = null;
+                return false;
+            }
+
+            result = ToValue(element);
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            return (IsArray)
+                ? TryGet(xml.Elements().ElementAtOrDefault((int)indexes[0]), out result)
+                : TryGet(xml.Element((string)indexes[0]), out result);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return (IsArray)
+                ? TryGet(xml.Elements().ElementAtOrDefault(int.Parse(binder.Name)), out result)
+                : TryGet(xml.Element(binder.Name), out result);
+        }
+
+        private bool TrySet(string name, object value)
+        {
+            var type = GetJsonType(value);
+            var element = xml.Element(name);
+            if (element == null)
+            {
+                xml.Add(new XElement(name, CreateTypeAttr(type), CreateJsonNode(value)));
+            }
+            else
+            {
+                element.Attribute("type").Value = type.ToString();
+                element.ReplaceNodes(CreateJsonNode(value));
+            }
+
+            return true;
+        }
+
+        private bool TrySet(int index, object value)
+        {
+            var type = GetJsonType(value);
+            var e = xml.Elements().ElementAtOrDefault(index);
+            if (e == null)
+            {
+                xml.Add(new XElement("item", CreateTypeAttr(type), CreateJsonNode(value)));
+            }
+            else
+            {
+                e.Attribute("type").Value = type.ToString();
+                e.ReplaceNodes(CreateJsonNode(value));
+            }
+
+            return true;
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            return (IsArray)
+                ? TrySet((int)indexes[0], value)
+                : TrySet((string)indexes[0], value);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return (IsArray)
+                ? TrySet(int.Parse(binder.Name), value)
+                : TrySet(binder.Name, value);
+        }
+
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            if (!IsArray || binder.Type != typeof(object[]))
+            {
+                result = null;
+                return false;
+            }
+
+            result = xml.Elements().Select(x => ToValue(x)).Cast<object>().ToArray();
+            return true;
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return (jsonType == JsonType.@object)
+                ? xml.Elements().Select(x => x.Name.LocalName)
+                : xml.Elements().Select((x, i) => i.ToString());
+        }
+
+        /// <summary>Serialize to JsonString</summary>
+        public override string ToString()
+        {
+            // <foo type="null"></foo> is can't serialize. replace to <foo type="null" />
+            foreach (var elem in xml.Descendants().Where(x => x.Attribute("type").Value == "null"))
+            {
+                elem.RemoveNodes();
+            }
+            return CreateJsonString(new XStreamingElement("root", CreateTypeAttr(jsonType), xml.Elements()));
+        }
     }
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,76 @@
-﻿
+﻿/*--------------------------------------------------------------------------
+* DynamicJson
+* ver 1.0.0.0 (Apr. 30th, 2010)
+*
+* created and maintained by neuecc <ils@neue.cc>
+* licensed under Microsoft Public License(Ms-PL)
+* http://neue.cc/
+* http://dynamicjson.codeplex.com/
+*--------------------------------------------------------------------------*/
+
+// InstallGuide
+
+Add Reference DynamicJson.dll
+or
+include DynamicJson.cs and Add Reference "System.Runtime.Serialization"
+
+// HowToUse
+
+using Codeplex.Data;
+
+// Parse (from JsonString to DynamicJson)
+var json = DynamicJson.Parse(@"{""foo"":""json"", ""bar"":100, ""nest"":{ ""foobar"":true } }");
+
+var r1 = json.foo; // "json" - dynamic(string)
+var r2 = json.bar; // 100 - dynamic(double)
+var r3 = json.nest.foobar; // true - dynamic(bool)
+var r4 = json["nest"]["foobar"]; // can access indexer
+
+// Check Defined
+var b1 = json.IsDefined("foo"); // true
+var b2 = json.IsDefined("foooo"); // false
+
+// Operate
+json.Arr = new string[] { "NOR", "XOR" }; // Add
+json.foo = 5000; // Replace
+json.Delete("bar"); // Delete
+            
+// Serialize
+var reJson = json.ToString(); // {"foo":5000,"nest":{"foobar":true},"Arr":["NOR","XOR"]}
+
+// Access Array
+Console.WriteLine(json.Arr[1]); // XOR
+foreach (var item in (object[])json.Arr) // please cast object[]
+    Console.WriteLine(item); // NOR XOR
+
+// Serialize (from Object to JsonString)
+var obj = new
+{
+    Name = "Foo",
+    Age = 30,
+    Address = new
+    {
+        Country = "Japan",
+        City = "Tokyo"
+    },
+    Like = new[] { "Microsoft", "Xbox" }
+};
+// {"Name":"Foo","Age":30,"Address":{"Country":"Japan","City":"Tokyo"},"Like":["Microsoft","Xbox"]}
+var jsonStringFromObj = DynamicJson.Serialize(obj);
+
+// Composite
+dynamic root = new DynamicJson(); // create root json container
+root.obj = new { }; // add blank object
+root.obj.str = "aaa";
+root.obj.@bool = true; // if propertyName is C#'s Reserved word then put prefix "@"
+root.array = new[] { 1, 200 }; // add array
+root.obj2 = new { str2 = "bbbb", ar = new object[] { "foobar", null, 100 } }; // add object and init
+
+// {"obj":{"str":"aaa","bool":true},"array":[1,200],"obj2":{"str2":"bbbb","ar":["foobar",null,100]}}
+Console.WriteLine(root.ToString()); 
+
+
+// History
+
+2010-04-30 ver 1.0.0.0
+1st Release
